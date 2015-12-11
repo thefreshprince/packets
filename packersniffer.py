7aa9c2d855a3533073702f624df65d8d609688ec
Ubuntu <corey@thefreshprince.thefreshprince.g2.internal.cloudapp.net>
2015-12-11 23:20:26 +0000

    ethernet layer

--- packersniffer.py
+++ packersniffer.py
@@ -8,3 +8,10 @@ import binascii
 rawSocket=socket.socket(socket.PF_PACKET,socket.SOCK_RAW,socket.htons(0x0800))
 #ifconfig eth0 promisc up
 receivedPacket=rawSocket.recv(2048)
+
+#Ethernet Header
+ethernetHeader=receivedPacket[0:14]
+ethrheader=struct.unpack("!6s6s2s",ethernetHeader)
+destinationIP= binascii.hexlify(ethrheader[0])
+sourceIP= binascii.hexlify(ethrheader[1])
+protocol= binascii.hexlify(ethrheader[2])
